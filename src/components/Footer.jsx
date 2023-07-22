06af34c797c2612cf51760c96e26db780ecb1674
shalinisangeet4680 <shalinisangeet35@gmail.com>
2023-07-22 19:49:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+        <div>
+        <a href= "https://www.youtube.com/channel/UCXzuuZWMwsSazHalgLTzU9Q" target={"blank"}>
+            YouTube
+            </a>
+        <a href= "https://www.instagram.com/shalinisangeet/?igshid=MzNlNGNkZWQ4Mg%3D%3D" target={"blank"}>
+            Instagram
+            </a>
+        <a href ="https://github.com/shalinisangeet4680" target={"blank"}>
+            Github
+            </a>
+        </div>
+        </div>
+
+    </footer>
+   
+  )
+}
+
+export default Footer;
